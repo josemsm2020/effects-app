d9c331c505356308da165d51c654b12b683e5e17
Yambo <yambo2005@hotmail.com>
2023-05-14 11:02:35 +0200

    Primer commit app sin effects

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private url = 'https://reqres.in/api';
+
+  constructor( private http: HttpClient) { }
+
+  getUsers(){
+    // con esta línea aparte de la lista de usuarios me devuelve más datos de información
+    //return this.http.get(`${ this.url }/users?per_page=6`); 
+    //Si escribo las siguientes líneas me devuelve la lista de usuarios 
+      return this.http.get(`${ this.url }/users?per_page=6`)
+      .pipe(
+        map( (resp: any) => {
+          return resp ['data'];
+        })                
+      );
+  }
+}
